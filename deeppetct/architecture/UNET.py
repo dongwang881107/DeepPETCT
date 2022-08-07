fce943d329e4ff230214369185f5266c94c83709
dongwang881107 <dongwang@seu.edu.cn>
2022-08-08 00:27:39 +0800

    unet variant

--- deeppetct/architecture/UNET.py
+++ deeppetct/architecture/UNET.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 import sys
 
 def get_acti(acti):
@@ -7,53 +8,163 @@ def get_acti(acti):
         ['leaky_relu', nn.LeakyReLU(negative_slope=0.01)],
     ])[acti]
 
-class UNET(nn.Module):
+class UNET_MP(nn.Module):
+    # Ref: Anatomically aided PET image reconstruction using deep neural networks
+    # Page 5, Figure 2(b)
+    # Conv2d(s=2) + ConvTranspose2d(s=2)
+    # Not working, has overlapping artifacts
     def __init__(self):
-        super(UNET, self).__init__()
+        super(UNET_MP, self).__init__()
         # mode, in_channel, out_channel, kernel_size, stride, padding, acti
-        self.cb1  = ConvBlock('conv', 2,  16, 3, 1, 1,'relu')
-        self.cb2  = ConvBlock('conv', 16, 16, 3, 2, 1,'relu')
-        self.cb3  = ConvBlock('conv', 16, 32, 3, 1, 1,'relu')
-        self.cb4  = ConvBlock('conv', 32, 32, 3, 2, 1,'relu')
-        self.cb5  = ConvBlock('conv', 32, 64, 3, 1, 1,'relu')
-        self.cb6  = ConvBlock('conv', 64, 128,3, 2, 1,'relu')
-
-        self.cb7  = ConvBlock('trans',128,64, 3, 2, [1,1],'relu')
-        self.cb8  = ConvBlock('conv', 64, 64, 3, 1, 1,'relu')
-        self.cb9  = ConvBlock('conv', 64, 64, 3, 1, 1,'relu')
-        self.cb10 = ConvBlock('trans',64, 32, 3, 2, [1,1],'relu')
-        self.cb11 = ConvBlock('conv', 32, 32, 3, 1, 1,'relu')
-        self.cb12 = ConvBlock('conv', 32, 32, 3, 1, 1,'relu')      
-        self.cb13 = ConvBlock('trans',32, 16, 3, 2, [1,1],'relu')
-        self.cb14 = ConvBlock('conv', 16, 16, 3, 1, 1,'relu')
-        self.cb15 = ConvBlock('conv', 16, 16, 3, 1, 1,'relu')
-        self.cb16 = ConvBlock('conv', 16, 1,  3, 1, 1,'relu')
-
+        self.kernel_size = 7
+        self.padding = 3
+        self.acti = 'relu'
+        # encoder
+        self.layer1 = ConvBlock('conv',2,16,self.kernel_size,1,self.padding,self.acti)
+        self.layer2 = Down('conv',16,16,self.kernel_size,2,self.padding,self.acti)
+        self.layer3 = ConvBlock('conv',16,32,self.kernel_size,1,self.padding,self.acti)
+        self.layer4 = Down('conv',32,32,self.kernel_size,2,self.padding,self.acti)
+        self.layer5 = ConvBlock('conv',32,64,self.kernel_size,1,self.padding,self.acti)
+        self.layer6 = Down('conv',64,128,self.kernel_size,2,self.padding,self.acti)
+        # decoder
+        self.layer7 = Up('trans',128,64,self.kernel_size,2,self.padding,self.acti)
+        self.layer8 = ConvBlock('conv',64,64,self.kernel_size,1,self.padding,self.acti)
+        self.layer9 = ConvBlock('conv',64,64,self.kernel_size,1,self.padding,self.acti)
+        self.layer10 = Up('trans',64,32,self.kernel_size,2,self.padding,self.acti)
+        self.layer11 = ConvBlock('conv',32,32,self.kernel_size,1,self.padding,self.acti)
+        self.layer12 = ConvBlock('conv',32,32,self.kernel_size,1,self.padding,self.acti)
+        self.layer13 = Up('trans',32,16,self.kernel_size,2,self.padding,self.acti)    
+        self.layer14 = ConvBlock('conv',16,16,self.kernel_size,1,self.padding,self.acti)
+        self.layer15 = ConvBlock('conv',16,16,self.kernel_size,1,self.padding,self.acti)
+        self.layer16 = ConvBlock('conv',16,1,self.kernel_size,1,self.padding,self.acti)
 
     def forward(self, x):
-        out = self.cb1(x)
+        out = self.layer1(x)
         res1 = out
-        out = self.cb2(out)
-        out = self.cb3(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
         res2 = out
-        out = self.cb4(out)
-        out = self.cb5(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
         res3 = out
-        out = self.cb6(out)
-        out = self.cb7(out)
+        out = self.layer6(out) 
+        out = self.layer7(out) 
         out = out + res3
-        out = self.cb8(out)
-        out = self.cb9(out)
-        out = self.cb10(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
         out = out + res2
-        out = self.cb11(out)
-        out = self.cb12(out)
-        out = self.cb13(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
         out = out + res1
-        out = self.cb14(out)
-        out = self.cb15(out)
-        out = self.cb16(out)
+        out = self.layer14(out)
+        out = self.layer15(out)
+        out = self.layer16(out)
         return out
+
+    @ classmethod
+    def compute_loss(cls):
+        return nn.MSELoss(reduction='sum')
+
+
+class UNET_MIA(nn.Module):
+    # Ref: Towards lower-dose PET using physics-based uncertainty-aware 
+    # multimodal learning with robustness to out-of-distribution data
+    # Page 102187, Figure 2
+    # Maxpooling(s=2) + Upsampling
+    def __init__(self):
+        super(UNET_MIA, self).__init__()
+        # mode, in_channel, out_channel, kernel_size, stride, padding, acti
+        self.kernel_size = 3
+        self.padding = 1
+        self.acti = 'relu'
+        # encoder
+        self.layer1 = ConvBlock('conv',2,1,1,1,0,self.acti)
+        self.layer2 = ConvBlock('conv',1,64,self.kernel_size,1,self.padding,self.acti)
+        self.layer3 = ConvBlock('conv',64,64,self.kernel_size,1,self.padding,self.acti)
+        self.layer4 = Down('maxpool',kernel_size=2,stride=2,padding=0)
+        self.layer5 = ConvBlock('conv',64,128,self.kernel_size,1,self.padding,self.acti)
+        self.layer6 = ConvBlock('conv',128,128,self.kernel_size,1,self.padding,self.acti)
+        self.layer7 = Down('maxpool',kernel_size=2,stride=2,padding=0)
+        self.layer8 = ConvBlock('conv',128,256,self.kernel_size,1,self.padding,self.acti)
+        self.layer9 = ConvBlock('conv',256,256,self.kernel_size,1,self.padding,self.acti)
+        self.layer10 = Down('maxpool',kernel_size=2,stride=2,padding=0)
+        self.layer11 = ConvBlock('conv',256,512,self.kernel_size,1,self.padding,self.acti)
+        self.layer12 = ConvBlock('conv',512,512,self.kernel_size,1,self.padding,self.acti)
+        self.layer13 = Down('maxpool',kernel_size=2,stride=2,padding=0)
+        self.layer14 = ConvBlock('conv',512,1024,self.kernel_size,1,self.padding,self.acti)
+        # decoder
+        self.layer15 = ConvBlock('conv',1024,512,self.kernel_size,1,self.padding,self.acti)
+        self.layer16 = Up('bilinear')
+        self.layer17 = ConvBlock('conv',1024,512,self.kernel_size,1,self.padding,self.acti)
+        self.layer18 = ConvBlock('conv',512,256,self.kernel_size,1,self.padding,self.acti)
+        self.layer19 = Up('bilinear')
+        self.layer20 = ConvBlock('conv',512,256,self.kernel_size,1,self.padding,self.acti)
+        self.layer21 = ConvBlock('conv',256,128,self.kernel_size,1,self.padding,self.acti)
+        self.layer22 = Up('bilinear')
+        self.layer23 = ConvBlock('conv',256,128,self.kernel_size,1,self.padding,self.acti)
+        self.layer24 = ConvBlock('conv',128,64,self.kernel_size,1,self.padding,self.acti)
+        self.layer25 = Up('bilinear')
+        self.layer26 = ConvBlock('conv',128,64,self.kernel_size,1,self.padding,self.acti)
+        self.layer27 = ConvBlock('conv',64,64,self.kernel_size,1,self.padding,self.acti)
+        self.layer28 = ConvBlock('conv',64,1,self.kernel_size,1,self.padding,self.acti)
+
+    def forward(self, x):
+        out = self.layer3(self.layer2(self.layer1(x)))
+        res1 = out
+        out = self.layer6(self.layer5(self.layer4(out)))
+        res2 = out
+        out = self.layer9(self.layer8(self.layer7(out)))
+        res3 = out
+        out = self.layer12(self.layer11(self.layer10(out)))
+        res4 = out
+        out = self.layer16(self.layer15(self.layer14(self.layer13(out))))
+        out = torch.cat([res4, out], dim=1)
+        out = self.layer19(self.layer18(self.layer17(out)))
+        out = torch.cat([res3, out], dim=1)
+        out = self.layer22(self.layer21(self.layer20(out)))
+        out = torch.cat([res2, out], dim=1)
+        out = self.layer25(self.layer24(self.layer23(out)))
+        out = torch.cat([res1, out], dim=1)
+        out = self.layer28(self.layer27(self.layer26(out)))
+        return out
+
+    @ classmethod
+    def compute_loss(cls):
+        return nn.MSELoss(reduction='sum')
+
+
+class UNET_TMI(nn.Module):
+    # 3D Auto-Context-Based Locality Adaptive Multi-Modality GANs for PET Synthesis
+    # Page 1311, Figure 3
+    # Conv2d(s=2) + Upsampling
+    def __init__(self):
+        super(UNET_TMI, self).__init__()
+        # mode, in_channel, out_channel, kernel_size, stride, padding, acti
+        self.kernel_size = 5
+        self.padding = 2
+        # encoder
+        self.cb1  = ConvBlock('conv', 2,  16, self.kernel_size, 1, self.padding,'relu')
+        self.cb2  = ConvBlock('conv', 16, 16, self.kernel_size, 2, self.padding,'relu')
+        self.cb3  = ConvBlock('conv', 16, 32, self.kernel_size, 1, self.padding,'relu')
+        self.cb4  = ConvBlock('conv', 32, 32, self.kernel_size, 2, self.padding,'relu')
+        self.cb5  = ConvBlock('conv', 32, 64, self.kernel_size, 1, self.padding,'relu')
+        self.cb6  = ConvBlock('conv', 64, 128,self.kernel_size, 2, self.padding,'relu')
+        # decoder
+        self.cb7  = ConvBlock('trans',128,64, self.kernel_size, 2, self.padding,'relu')
+        self.cb8  = ConvBlock('conv', 64, 64, self.kernel_size, 1, self.padding,'relu')
+        self.cb9  = ConvBlock('conv', 64, 64, self.kernel_size, 1, self.padding,'relu')
+        self.cb10 = ConvBlock('trans',64, 32, self.kernel_size, 2, self.padding,'relu')
+        self.cb11 = ConvBlock('conv', 32, 32, self.kernel_size, 1, self.padding,'relu')
+        self.cb12 = ConvBlock('conv', 32, 32, self.kernel_size, 1, self.padding,'relu')      
+        self.cb13 = ConvBlock('trans',32, 16, self.kernel_size, 2, self.padding,'relu')
+        self.cb14 = ConvBlock('conv', 16, 16, self.kernel_size, 1, self.padding,'relu')
+        self.cb15 = ConvBlock('conv', 16, 16, self.kernel_size, 1, self.padding,'relu')
+        self.cb16 = ConvBlock('conv', 16, 1,  self.kernel_size, 1, self.padding,'relu')
+
+    def forward(self):
+        return True
 
     @ classmethod
     def compute_loss(cls):
@@ -65,15 +176,12 @@ class ConvBlock(nn.Module):
         if mode == 'conv':
             self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
         elif mode == 'trans':
-            self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, \
-                padding=padding[0], output_padding=padding[1])
+            self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, output_padding=1)
         else:
-            print('[conv] or [trans] only!')
+            print('[conv] or [trans]')
             sys.exit(0)
-
         self.bn = nn.BatchNorm2d(out_channels)
         self.acti = get_acti(acti)
-
         self.blocks = nn.Sequential(
             self.conv,
             self.bn,
@@ -84,3 +192,30 @@ class ConvBlock(nn.Module):
         x = self.blocks(x)
         return x
 
+class Down(nn.Module):
+    def __init__(self, mode, in_channels=None, out_channels=None, kernel_size=None, stride=None, padding=None, acti=None):
+        super().__init__()
+        if mode == 'conv':
+            self.down = ConvBlock('conv', in_channels, out_channels, kernel_size, stride, padding, acti)
+        elif mode == 'maxpool':
+            self.down = nn.MaxPool2d(kernel_size, stride, padding)
+        else:
+            print('[conv] or [maxpool]')
+            sys.exit(0)
+
+    def forward(self, x):
+        return self.down(x)
+
+class Up(nn.Module):
+    def __init__(self, mode, in_channels=None, out_channels=None, kernel_size=None, stride=None, padding=None, acti=None):
+        super().__init__()
+        if mode == 'trans':
+            self.up = ConvBlock('trans', in_channels, out_channels, kernel_size, stride, padding, acti)
+        elif mode == 'bilinear':
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            print('[trans] or [bilinear]')
+            sys.exit(0)
+    
+    def forward(self, x):
+        return self.up(x)
--- deeppetct/architecture/__init__.py
+++ deeppetct/architecture/__init__.py
@@ -9,6 +9,6 @@ from . import SA_UNET
 
 redcnn = RED_CNN.RED_CNN
 saunet = SA_UNET.SA_UNET
-unet = UNET.UNET
+unet = UNET.UNET_MIA
 cycle = CycleGAN.CycleGAN
 wganvgg = WGAN_VGG.WGAN_VGG
