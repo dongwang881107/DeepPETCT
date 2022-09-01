08cfbc9af377225ab064f2461653b31cd367f09a
dongwang881107 <dongwang@seu.edu.cn>
2022-09-01 11:23:58 +0800

    structure-promoting loss

--- deeppetct/loss.py
+++ deeppetct/loss.py
@@ -6,6 +6,9 @@ from torch.autograd import Variable
 
 __all__ = [
     "SSIMLoss",
+    "TVLoss",
+    "wTVLoss",
+    "dTVLoss",
     "LossCompose",
 ]
 
@@ -69,7 +72,37 @@ class TVLoss(nn.Module):
     def __init__(self):
         super(TVLoss,self).__init__()
 
-    def forward(self,x):
+    def forward(self, x):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h = (x.size()[2]-1) * x.size()[3]
+        count_w = x.size()[2] * (x.size()[3]-1)
+        h_tv = torch.pow((x[:,:,1:,:]-x[:,:,:h_x-1,:]),2).sum()
+        w_tv = torch.pow((x[:,:,:,1:]-x[:,:,:,:w_x-1]),2).sum()
+        return 2*(h_tv/count_h+w_tv/count_w)/batch_size
+
+class wTVLoss(nn.Module):
+    # Total Variation (TV) Loss
+    def __init__(self):
+        super(TVLoss,self).__init__()
+
+    def forward(self, x, y):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h = (x.size()[2]-1) * x.size()[3]
+        count_w = x.size()[2] * (x.size()[3]-1)
+        h_tv = torch.pow((x[:,:,1:,:]-x[:,:,:h_x-1,:]),2).sum()
+        w_tv = torch.pow((x[:,:,:,1:]-x[:,:,:,:w_x-1]),2).sum()
+        return 2*(h_tv/count_h+w_tv/count_w)/batch_size
+
+class dTVLoss(nn.Module):
+    # Total Variation (TV) Loss
+    def __init__(self):
+        super(TVLoss,self).__init__()
+
+    def forward(self, x, y):
         batch_size = x.size()[0]
         h_x = x.size()[2]
         w_x = x.size()[3]
